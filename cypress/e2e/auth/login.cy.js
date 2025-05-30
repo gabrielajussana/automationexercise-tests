e23b33dd34f672727c18af4b00e1372764474c58
Gabriela Jussana <gabrielajussana@gmail.com>
2025-05-30 20:46:20 -0300

    chore: edit login tests

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -1,5 +1,5 @@
-describe('Login no AutomationExercise', () => {
-  it('Deve fazer login com sucesso', () => {
+describe('Login on AutomationExercise', () => {
+  it('Should log in successfully', () => {
     cy.visit('https://automationexercise.com/login')
 
     cy.get('input[data-qa="login-email"]').type('tests@tests.com')
@@ -10,28 +10,28 @@ describe('Login no AutomationExercise', () => {
     cy.contains('Logged in as').should('be.visible')
   })
 
-  it('Deve tentar fazer login com senha inválida', () => {
+  it('Should try to log in with an invalid password', () => {
     cy.visit('https://automationexercise.com/login')
 
     cy.get('input[data-qa="login-email"]').type('tests@tests.com')
     cy.get('input[data-qa="login-password"]').type('senhaerrada')
     cy.get('button[data-qa="login-button"]').click()
     cy.get('.alert-danger').should('contain', 'Your email or password is incorrect!')
-    })
+  })
 
-  it('Deve tentar fazer login com campos vazios', () => {
+  it('Should try to log in with empty fields', () => {
     cy.visit('https://automationexercise.com/login')
 
     cy.get('button[data-qa="login-button"]').click()
     cy.get('.alert-danger').should('contain', 'Email Address and Password are required!')
   })
 
-    it('Deve tentar fazer login com email inválido', () => {
-        cy.visit('https://automationexercise.com/login')
-    
-        cy.get('input[data-qa="login-email"]').type('emailinvalido')
-        cy.get('input[data-qa="login-password"]').type('teste123')
-        cy.get('button[data-qa="login-button"]').click()
-        cy.get('.alert-danger').should('contain', 'Invalid email address!')
-    })  
+  it('Should try to log in with an invalid email format', () => {
+    cy.visit('https://automationexercise.com/login')
+
+    cy.get('input[data-qa="login-email"]').type('emailinvalido')
+    cy.get('input[data-qa="login-password"]').type('teste123')
+    cy.get('button[data-qa="login-button"]').click()
+    cy.get('.alert-danger').should('contain', 'Invalid email address!')
+  })
 })
